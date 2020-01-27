dd083a8c019bdf1f7d431176eb7a538c28a21116
chronoreaper <chris.dd.29@gmail.com>
2020-01-27 16:42:39 -0500

    Added the implementation of the statuses
    Added FakeTestCaseStatus, Set and Step

---
+++ AutomationTestSetFrameworkNUnit/FakeTestCaseStatus.cs
@@ -0,0 +1,20 @@
+﻿using AutomationTestSetFramework;
+using static AutomationTestSetFramework.ITestStatus;
+using System;
+
+
+namespace AutomationTestSetFrameworkNUnit
+{
+    class FakeTestCaseStatus : ITestCaseStatus
+    {
+        public int TestCaseNumber { get; set; }
+        public bool RunSuccessful { get; set; }
+        public string ErrorStack { get; set; }
+        public string FriendlyErrorMessage { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string Description { get; set; }
+        public string Expected { get; set; }
+        public string Actual { get; set; }
+    }
+}
