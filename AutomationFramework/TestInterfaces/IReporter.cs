5f8f9aa29f294f26c260abb9c68028dfec91eb90
chronoreaper <chris.dd.29@gmail.com>
2020-01-27 14:17:25 -0500

    Create IReporter.cs

---
+++ AutomationFramework/TestInterfaces/IReporter.cs
@@ -0,0 +1,12 @@
+﻿// <copyright file="IReporter.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+namespace AutomationTestSetFramework
+{
+    /// <summary>
+    /// Interface for the reporter class. Reports to the tester the results usualy after a test step.
+    /// </summary>
+    interface IReporter
+    {
+    }
+}
