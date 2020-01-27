48abd835dea9e39598ff1e4160402bc6ccf3e99c
chronoreaper <chris.dd.29@gmail.com>
2020-01-27 14:25:14 -0500

    Update IReporter.cs

--- AutomationFramework/TestInterfaces/IReporter.cs
+++ AutomationFramework/TestInterfaces/IReporter.cs
@@ -6,7 +6,29 @@ namespace AutomationTestSetFramework
     /// <summary>
     /// Interface for the reporter class. Reports to the tester the results usualy after a test step.
     /// </summary>
-    interface IReporter
+    public interface IReporter
     {
+        /// <summary>
+        /// Sends the report to the tester.
+        /// </summary>
+        public void Report();
+
+        /// <summary>
+        /// Adds a status for a test set.
+        /// </summary>
+        /// <param name="testSetStatus">The test set to add a status for.</param>
+        public void AddTestSetStatus(ITestSetStatus testSetStatus);
+
+        /// <summary>
+        /// Adds a status for a test case.
+        /// </summary>
+        /// <param name="testCaseStatus">The test case to add a status for.</param>
+        public void AddTestCaseStatus(ITestCaseStatus testCaseStatus);
+
+        /// <summary>
+        /// Adds a status for a test step.
+        /// </summary>
+        /// <param name="testStepStatus">The test step to add a status for.</param>
+        public void AddTestStepStatus(ITestStepStatus testStepStatus);
     }
 }
