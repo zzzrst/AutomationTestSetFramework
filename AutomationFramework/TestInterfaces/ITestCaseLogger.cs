c21fc7a61bac46f0276dcd1b6ca7ef711475882d
chronoreaper <chris.dd.29@gmail.com>
2020-01-27 14:17:18 -0500

    Added the Test Loggers

---
+++ AutomationFramework/TestInterfaces/ITestCaseLogger.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="ITestStep.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+namespace AutomationTestSetFramework
+{
+    /// <summary>
+    /// Interface that represents the test case logger. It writes information about the test case.
+    /// </summary>
+    public interface ITestCaseLogger
+    {
+        /// <summary>
+        /// Writes to log information anout the test case.
+        /// </summary>
+        /// <param name="testCase">The test case to log.</param>
+        public void Log(ITestCase testCase);
+    }
+}
