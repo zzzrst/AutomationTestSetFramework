1f023319bb82a250270a19c94f3580781fd5c758
chronoreaper <chris.dd.29@gmail.com>
2020-01-27 15:23:56 -0500

    Update ITestCaseLogger.cs

--- AutomationFramework/TestInterfaces/ITestCaseLogger.cs
+++ AutomationFramework/TestInterfaces/ITestCaseLogger.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="ITestStep.cs" company="PlaceholderCompany">
+﻿// <copyright file="ITestCaseLogger.cs" company="PlaceholderCompany">
 // Copyright (c) PlaceholderCompany. All rights reserved.
 // </copyright>
 namespace AutomationTestSetFramework
