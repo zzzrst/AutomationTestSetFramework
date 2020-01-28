c07ab5f8b389115f3f5f23c42144541bf3993e93
chronoreaper <chris.dd.29@gmail.com>
2020-01-28 11:48:02 -0500

    Update RunTestSet.cs
    Started the implementation of the unit test

--- AutomationTestSetFrameworkNUnit/RunTestSet.cs
+++ AutomationTestSetFrameworkNUnit/RunTestSet.cs
@@ -1,12 +1,33 @@
+using AutomationTestSetFramework;
 using NUnit.Framework;
 
 namespace AutomationTestSetFrameworkNUnit
 {
     public class RunTestSetTest
     {
+        private ITestCase TestCase;
+        private ITestStep TestStep;
+        private ITestSet TestSet;
+
         [SetUp]
         public void Setup()
         {
+            TestStep = new FakeTestStep()
+            {
+                ShouldExecuteAmountOfTimes = 1,
+                Name = "Test Step",
+                NextRunRaiseException = false,
+                OnExceptionFlowBehavior = IMethodBoundaryAspect.FlowBehavior.Return,
+            };
+
+            TestCase = new FakeTestCase()
+            {
+                ShouldExecuteAmountOfTimes = 1,
+                Name = "Test Case",
+                NextRunRaiseException = false,
+                OnExceptionFlowBehavior = IMethodBoundaryAspect.FlowBehavior.Return,
+                TestStep = new List<ITestStep>() { TestStep },
+            };
         }
 
         [Test]
